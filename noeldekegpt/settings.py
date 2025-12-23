2fada29bb725c35a0b0a8ab10245e0bdaca09575
Nicolas Beglinger <nicolas.beglinger@laixo.ch>
2025-12-23 15:46:51 +0100

    Moved to uv

--- noeldekegpt/settings.py
+++ noeldekegpt/settings.py
@@ -30,7 +30,7 @@ STATICFILES_DIRS = [
 SECRET_KEY = "django-insecure-15ub0v16g25elwig-mo939uzuz&&engejpixili-yxy-diig6="
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["nbeglinger.pythonanywhere.com", "127.0.0.1"]
 
